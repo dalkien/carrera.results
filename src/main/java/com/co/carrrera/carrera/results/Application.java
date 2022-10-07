7500ac39a43a4404a6cec88be19bd3ed99f75520
Dalkien <darktsubzero@gmail.com>
2022-10-06 23:02:18 -0500

    Initial commit

---
+++ src/main/java/com/co/carrrera/carrera/results/Application.java
@@ -0,0 +1,13 @@
+package com.co.carrrera.carrera.results;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/io/spring/guides/gs_producing_web_service/CarreraResponse.java
@@ -0,0 +1,127 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2022.10.06 a las 10:47:31 PM COT 
+//
+
+
+package io.spring.guides.gs_producing_web_service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="code" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="message" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="datosResultado" type="{http://spring.io/guides/gs-producing-web-service}resultadoInfo"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "code",
+    "message",
+    "datosResultado"
+})
+@XmlRootElement(name = "carreraResponse")
+public class CarreraResponse {
+
+    @XmlElement(required = true)
+    protected String code;
+    @XmlElement(required = true)
+    protected String message;
+    @XmlElement(required = true)
+    protected ResultadoInfo datosResultado;
+
+    /**
+     * Obtiene el valor de la propiedad code.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Define el valor de la propiedad code.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCode(String value) {
+        this.code = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad message.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Define el valor de la propiedad message.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMessage(String value) {
+        this.message = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad datosResultado.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ResultadoInfo }
+     *     
+     */
+    public ResultadoInfo getDatosResultado() {
+        return datosResultado;
+    }
+
+    /**
+     * Define el valor de la propiedad datosResultado.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ResultadoInfo }
+     *     
+     */
+    public void setDatosResultado(ResultadoInfo value) {
+        this.datosResultado = value;
+    }
+
+}
